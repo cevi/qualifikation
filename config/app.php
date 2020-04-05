c91e5c4443b20238f3f5f86fa2cbe92c35c25547
jerome.sigg <jerome.sigg@gmail.com>
2020-04-05 20:47:42 +0200

    Froiden installed

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
+        Froiden\LaravelInstaller\Providers\LaravelInstallerServiceProvider::class,
 
         /*
          * Package Service Providers...
