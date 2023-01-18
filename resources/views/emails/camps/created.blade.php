6a846c6b244cb3de2ed3587a2d2c0bb1eee75f7a
jerome.sigg <jerome.sigg@gmail.com>
2023-01-18 22:04:08 +0100

    More Details in Email

--- resources/views/emails/camps/created.blade.php
+++ resources/views/emails/camps/created.blade.php
@@ -1,10 +1,11 @@
 @component('mail::message')
-# Du hast einen neuen Kurs erstellt.
+# Hallo {{$camp->user['username']}}, Du hast einen Kurs erstellt.
 
-Viel Erfolg bei deinem Kurs. Solltest du noch Fragen haben, dann schaue auf dem Wiki oder melde dich bei <a href="mailto:{{config('mail.from.address')}}">uns.</a>
+Viel Erfolg bei deinem Kurs {{$camp['name']}} ({{$camp->camp_type['name']}}) für die Region {{$camp->group['name']}}. Solltest du noch Fragen haben, dann schaue auf dem Wiki oder melde dich bei
+<a href="mailto:{{config('mail.from.address')}}">uns.</a>
 
 @component('mail::button', ['url' => 'https://github.com/cevi/qualifikation/wiki'])
-Wiki
+    Wiki
 @endcomponent
 
 Vielen Dank und viel Spass,<br>
