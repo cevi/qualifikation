cd251e2749e9252b47ba37c417d986268af6cf14
jerome.sigg <jerome.sigg+newsletter@gmail.com>
2025-02-26 23:08:57 +0100

    Profilbild korrigiert, Modal für Flowbite angepasst

--- resources/views/components/user-dropdown.blade.php
+++ resources/views/components/user-dropdown.blade.php
@@ -1,11 +1,5 @@
 @auth
-    <button
-        type="button"
-        class="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-        id="user-menu-button"
-        aria-expanded="false"
-        data-dropdown-toggle="dropdown"
-    >
+    <button type="button" class="min-w-8 flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="dropdown">
         <span class="sr-only">Usermenü öffnen</span>
         <img
             class="w-8 h-8 rounded-full"
@@ -19,25 +13,12 @@
     id="dropdown"
     >
         <div class="py-3 px-4">
-            <span
-            class="block text-sm font-semibold text-gray-900 dark:text-white"
-            > {{ Auth::user()->username }} </span
-            >
-            <span
-            class="block text-sm text-gray-900 truncate dark:text-white"
-            > {{ Auth::user()->email }} </span
-            >
+            <span class="block text-sm font-semibold text-gray-900 dark:text-white"> {{ Auth::user()->username }} </span>
+            <span class="block text-sm text-gray-900 truncate dark:text-white"> {{ Auth::user()->email }} </span>
         </div>
-        <ul
-            class="py-1 text-gray-700 dark:text-gray-300"
-            aria-labelledby="dropdown"
-        >
+        <ul class="py-1 text-gray-700 dark:text-gray-300" aria-labelledby="dropdown">
             <li>
-            <a
-                href="{{ route('home.user',Auth::user()->slug) }}"
-                class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
-                >Profil</a
-            >
+                <a href="{{ route('home.user',Auth::user()->slug) }}" class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white" >Profil</a>
             </li>
         </ul>
         <ul
--- resources/views/includes/topnav.blade.php
+++ resources/views/includes/topnav.blade.php
@@ -1,68 +1,81 @@
-<nav class="dark:bg-gray-800 bg-gray-100 border-b border-gray-200 px-4 lg:px-6 py-2.5 dark:border-gray-700 fixed left-0 right-0 top-0 z-50">
-    <div class="flex flex-wrap justify-between items-center">
-        <div class="flex justify-start items-center">
-            @auth
-                <a class="navbar-brand" href="{{ url('/home') }}"  class="flex items-center">
-                    <img src="/img/logo.svg" alt="QualiTool Logo" class="mr-3 h-6 lg:h-12">
-                </a>
-            @else
-                <a class="navbar-brand" href="{{ url('/') }}"  class="flex items-center">
-                    <img src="/img/logo.svg" alt="QualiTool Logo" class="mr-3 h-6 lg:h-12">
-                </a>
-            @endauth
-        </div>
-        <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
-            <ul class="flex flex-col font-medium lg:flex-row lg:space-x-8">
+<header class="flex flex-col antialiased">
+      <nav class="dark:bg-gray-800 bg-gray-100 border-b border-gray-200 px-4 lg:px-6 py-2.5 dark:border-gray-700 order-1">
+        <div class="flex justify-between items-center">
+
+            <div class="flex flex-shrink-0 justify-start items-center">
                 @auth
-                @if (Auth::user()->isCampleader())
-                    <li>
-                        <a href="/admin"
-                        type="button"
-                        class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                        >
-                            <span class="flex-1 ml-3 text-left whitespace-nowrap">       
-                                Dashboard
-                            </span>
-                        </a>    
-                    </li>
-                @endif
-                @if (!Auth::user()->isTeilnehmer())
-                    @isset($camp)
-                        @if($camp->participants()->count()>0)
-                            <li>
-                                <button
-                                    type="button"
-                                    class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                                    id="users-menu-button"
-                                    aria-expanded="false"
-                                    data-dropdown-toggle="dropdown-users"
-                                    >
-                                    <span class="flex-1 ml-3 text-left whitespace-nowrap">            
-                                        Teilnehmer
-                                    </span>
-                                    <svg
-                                        aria-hidden="true"
-                                        class="w-6 h-6"
-                                        fill="currentColor"
-                                        viewBox="0 0 20 20"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            fill-rule="evenodd"
-                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                            clip-rule="evenodd">
-                                        </path>
-                                    </svg>
-                                </button>
-                            </li>
-                            <div
-                                class="hidden z-50 my-4 w-56 text-base list-none navbar-background rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
-                                id="dropdown-users"
-                            >
-                            
-                                @if(count($camp->my_participants)>0)
-                                    <ul aria-labelledby="dropdown-users" class="py-1 text-gray-700 dark:text-gray-300">
-                                        @foreach ($camp->my_participants as $user_profile)
+                    <a class="navbar-brand" href="{{ url('/home') }}"  class="flex items-center">
+                        <x-logo/>
+                    </a>
+                @else
+                    <a class="navbar-brand" href="{{ url('/') }}"  class="flex items-center">
+                        <x-logo/>
+                    </a>
+                @endauth
+            </div>
+
+            <ul class="hidden flex-col justify-center mt-0 w-full text-sm font-medium text-gray-500 md:flex-row dark:text-gray-400 md:flex">
+                @auth
+                    @if (Auth::user()->isCampleader())
+                        <li>
+                            <a href="/admin" type="button" class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">
+                                <span class="flex-1 ml-3 text-left whitespace-nowrap">       
+                                    Dashboard
+                                </span>
+                            </a>    
+                        </li>
+                    @endif
+                    @if (!Auth::user()->isTeilnehmer())
+                        @isset($camp)
+                            @if($camp->participants()->count()>0)
+                                <li>
+                                    <button
+                                        type="button"
+                                        class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                                        id="users-menu-button"
+                                        aria-expanded="false"
+                                        data-dropdown-toggle="dropdown-users"
+                                        >
+                                        <span class="flex-1 ml-3 text-left whitespace-nowrap">            
+                                            Teilnehmer
+                                        </span>
+                                        <svg
+                                            aria-hidden="true"
+                                            class="w-6 h-6"
+                                            fill="currentColor"
+                                            viewBox="0 0 20 20"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd">
+                                            </path>
+                                        </svg>
+                                    </button>
+                                </li>
+                                <div
+                                    class="hidden z-50 my-4 w-56 text-base list-none navbar-background divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
+                                    id="dropdown-users"
+                                >
+                                
+                                    @if(count($camp->my_participants)>0)
+                                        <ul aria-labelledby="dropdown-users" class="py-1 text-gray-700 dark:text-gray-300">
+                                            @foreach ($camp->my_participants as $user_profile)
+                                                <li>
+                                                    <a class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
+                                                        href="{{route('home.profile', $user_profile->slug)}}">{{$user_profile->leader_id === Auth::user()->id ? '*' : ''}}{{$user_profile->username}}</a>
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    @endif
+                                    
+                                    @if(count($camp->other_participants)>0 && count($camp->my_participants)>0)
+                                    <hr>
+                                    @endif
+                                    @if(count($camp->other_participants)>0)
+                                    <ul aria-labelledby="dropdown-users" class="h-dropdown py-1 text-gray-700 dark:text-gray-300 overflow-y-auto">
+                                        @foreach ($camp->other_participants as $user_profile)
                                             <li>
                                                 <a class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
                                                     href="{{route('home.profile', $user_profile->slug)}}">{{$user_profile->leader_id === Auth::user()->id ? '*' : ''}}{{$user_profile->username}}</a>
@@ -70,37 +83,25 @@
                                         @endforeach
                                     </ul>
                                 @endif
-                                
-                                @if(count($camp->other_participants)>0 && count($camp->my_participants)>0)
-                                <hr>
-                                @endif
-                                @if(count($camp->other_participants)>0)
-                                <ul aria-labelledby="dropdown-users" class="h-dropdown py-1 text-gray-700 dark:text-gray-300 overflow-y-auto">
-                                    @foreach ($camp->other_participants as $user_profile)
-                                        <li>
-                                            <a class="block py-2 px-4 text-sm hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
-                                                href="{{route('home.profile', $user_profile->slug)}}">{{$user_profile->leader_id === Auth::user()->id ? '*' : ''}}{{$user_profile->username}}</a>
-                                        </li>
-                                    @endforeach
-                                </ul>
+                                </div>
                             @endif
-                            </div>
-                        @endif
-                    @endisset
-                    <li>
-                        <a href="/post"
-                        type="button"
-                        class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                        >
-                            <span class="flex-1 ml-3 text-left whitespace-nowrap">       
-                                Rückmeldungen
-                            </span>
-                        </a>
-                    </li>
-                @endif
-            @endauth
+                        @endisset
+                        <li>
+                            <a href="/post"
+                            type="button"
+                            class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                            >
+                                <span class="flex-1 ml-3 text-left whitespace-nowrap">       
+                                    Rückmeldungen
+                                </span>
+                            </a>
+                        </li>
+                    @endif
+                @endauth
             </ul>
+
+            
+            <x-right-navbar/>
         </div>
-        <x-right-navbar/>
-    </div>
-</nav>
+    </nav>
+</header>
