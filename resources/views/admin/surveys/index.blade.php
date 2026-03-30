20a0b74c6e6a9d6bc663cd7b09ccc3e53801139d
Cyrill Püntener <wp99cp@gmx.net>
2026-03-30 11:17:47 +0200

    feat: disable survey button if no qualifications are available

--- resources/views/admin/surveys/index.blade.php
+++ resources/views/admin/surveys/index.blade.php
@@ -13,7 +13,13 @@
             </div>
             @if($camp['status_control'] && $camp['survey_status_id'] < config('status.survey_1offen'))
                 <div class="col-sm-4" style="margin-bottom: 10px;">
-                    <a href="javascript:;" class="focus:outline-hidden text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 opensurvey" role="button">Erste Selbsteinschätzung freigeben</a>
+                    @if($camp->surveys()->count() > 0)
+                        <a href="javascript:;" class="focus:outline-hidden text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 opensurvey" role="button">Erste Selbsteinschätzung freigeben</a>
+                    @else
+                        <span title="Bitte zuerst auf 'Qualifikationen erstellen' klicken" style="display: inline-block; cursor: not-allowed;">
+                            <a href="javascript:;" class="focus:outline-hidden text-white bg-purple-700 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600" style="pointer-events: none; opacity: 0.5; text-decoration: none; display: inline-block;" role="button" aria-disabled="true">Erste Selbsteinschätzung freigeben</a>
+                        </span>
+                    @endif
                 </div>
             @else
                 @if(!$camp['secondsurveyopen'])
