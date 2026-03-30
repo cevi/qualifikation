ab7009802aeabc518a108e9c2893be4e70ea1f84
Cyrill Püntener <wp99cp@gmx.net>
2026-03-30 12:29:08 +0200

    fix: disable tab selection on disabled button

--- resources/views/components/survey-button.blade.php
+++ resources/views/components/survey-button.blade.php
@@ -11,7 +11,7 @@
         <a href="{{ $url }}" target="{{ $target }}" class="focus:outline-hidden text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900 {{ $btnClass }}" role="button">{{ $text }}</a>
     @else
         <span title="Bitte zuerst auf 'Qualifikationen erstellen' klicken" style="display: inline-block; cursor: not-allowed;">
-            <a href="javascript:;" class="focus:outline-hidden text-white bg-purple-700 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 {{ $btnClass }}" style="pointer-events: none; opacity: 0.5; text-decoration: none; display: inline-block;" role="button" aria-disabled="true">{{ $text }}</a>
+            <button disabled class="focus:outline-hidden text-white bg-purple-700 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600" style="pointer-events: none; opacity: 0.5; display: inline-block;">{{ $text }}</button>
         </span>
     @endif
 </div>
